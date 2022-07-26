ebdd6b12b6bc954afa969c6e4b85be6227e49463
recipra <jmo0425@outlook.com>
2022-07-26 14:20:44 -0400

    adjust profile model

--- models/profile.js
+++ models/profile.js
@@ -3,7 +3,8 @@ import mongoose from 'mongoose'
 const profileSchema = new mongoose.Schema({
   email: { type: String, required: true, lowercase: true, unique: true },
   name: String,
-  photo: { type: String }
+  photo: { type: String },
+  assignments: [{type: mongoose.Schema.Types.ObjectId, ref: 'Assignment'}]
 },{
   timestamps: true,
 })
